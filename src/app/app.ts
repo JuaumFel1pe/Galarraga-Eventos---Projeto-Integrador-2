a5842e1abd47752dc0251dcd5dad5f25a67c5746
João Michael <160083859+JmZgerr95@users.noreply.github.com>
2026-04-05 14:12:10 -0300

    Sessao 6 finalizada

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Cabecalho } from './components/cabecalho/cabecalho';
 import { Rodape } from './components/rodape/rodape';
+import { Sessao6 } from './components/sessao6/sessao6';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Cabecalho, Rodape],
+  imports: [RouterOutlet, Cabecalho, Rodape, Sessao6],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
